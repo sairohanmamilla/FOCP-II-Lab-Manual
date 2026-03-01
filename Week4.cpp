557c94d9ce394dca0e5e86b7e082468afc1a2123
sairohanmamilla <sairohan.mamilla@gmail.com>
2026-03-01 11:05:22 +0530

    Create Week4.cpp
    The file contains commented-out code examples for calculating the area of a triangle, array traversal, and a pattern printing program using classes and objects.

---
+++ Week4.cpp
@@ -0,0 +1,69 @@
+//Calculate area of triangle using classes and objects
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+/*class triangleArea {
+    private:
+        float base;
+        float height;
+    public:
+        void setData(float b, float h) {
+            base = b;
+            height = h;
+        }
+        float calculateArea() {
+            return 0.5 * base * height;
+        }
+};
+
+int main() {
+
+    triangleArea t1;
+    t1.setData(30, 20);
+    t1.calculateArea();
+
+    return 0;
+} */
+
+//Array traversal using classes and objects
+/*class traverse {
+    private:
+        int arr[10] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+    public:
+        void arrTraversal() {
+            for(int i = 0; i < 10; i++) {
+                cout << arr[i] << endl;
+            }
+        }
+};
+
+int main () {
+
+
+    return 0;
+} */
+
+/*#include <iostream>
+using namespace std;
+int main() 
+{
+    int n=4;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int space = 1; space <= n-i; space++)
+        {
+            cout << "";
+        }
+        for(int j = 1; j <= i; j++) 
+        {
+            cout << j;
+        }
+        for(int j = i; j >= 1; j--) 
+        {
+            cout << j;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}*/
